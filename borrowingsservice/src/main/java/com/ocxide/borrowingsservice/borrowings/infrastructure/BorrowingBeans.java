5e98d9c2dda82216d9a2df833e17c786e757b201
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2025-07-26 14:07:18 -0500

    feat(borrowings): kafka notificator

--- borrowingsservice/src/main/java/com/ocxide/borrowingsservice/borrowings/infrastructure/BorrowingBeans.java
+++ borrowingsservice/src/main/java/com/ocxide/borrowingsservice/borrowings/infrastructure/BorrowingBeans.java
@@ -5,10 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.ocxide.borrowingsservice.application.CreateOneUseCase;
+import com.ocxide.borrowingsservice.domain.BookCopiesRepository;
+import com.ocxide.borrowingsservice.domain.BorrowingsNotificator;
+import com.ocxide.borrowingsservice.domain.BorrowingsRepository;
+
 @Configuration
 public class BorrowingBeans {
 	@Bean("booksservice")
 	WebClient webClient(WebClient.Builder builder, @Value("${booksservice.uri}") String uri) {
 		return builder.defaultHeader("Content-Type", "application/json").baseUrl(uri).build();
+	}
+
+	@Bean
+	CreateOneUseCase createOneUseCase(
+			BookCopiesRepository bookCopiesRepository,
+			BorrowingsRepository borrowingsRepository,
+			BorrowingsNotificator notificator) {
+		return new CreateOneUseCase(bookCopiesRepository, borrowingsRepository, notificator);
 	}
 }
---
+++ borrowingsservice/src/main/java/com/ocxide/borrowingsservice/borrowings/infrastructure/kafka/KafkaBorrowingsNotificator.java
@@ -0,0 +1,23 @@
+package com.ocxide.borrowingsservice.borrowings.infrastructure.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.ocxide.borrowingsservice.domain.Borrowing;
+import com.ocxide.borrowingsservice.domain.BorrowingsNotificator;
+
+import lombok.AllArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class KafkaBorrowingsNotificator implements BorrowingsNotificator {
+	private final KafkaTemplate<String, Borrowing> kafkaTemplate;
+
+	@Override
+	public Mono<Void> onCopyBorrowed(Borrowing borrowing) {
+		var fut = kafkaTemplate.send("borrow", borrowing);
+		return Mono.fromFuture(fut).then();
+	}
+
+}
