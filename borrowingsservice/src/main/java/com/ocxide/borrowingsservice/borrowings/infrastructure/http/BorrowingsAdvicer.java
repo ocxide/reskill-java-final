c002fbe177e6c586b7bdcd47f3444443ab48afa7
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2025-07-26 18:15:38 -0500

    fix(borrowings): handle borrowings errors

---
+++ borrowingsservice/src/main/java/com/ocxide/borrowingsservice/borrowings/infrastructure/http/BorrowingsAdvicer.java
@@ -0,0 +1,23 @@
+package com.ocxide.borrowingsservice.borrowings.infrastructure.http;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+import com.ocxide.borrowingsservice.domain.BookCopyNotAvailable;
+import com.ocxide.borrowingsservice.domain.BookCopyNotFound;
+
+@ControllerAdvice
+public class BorrowingsAdvicer {
+
+	@ExceptionHandler(BookCopyNotFound.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void handleBookNotFound(BookCopyNotFound e) {
+	}
+
+	@ExceptionHandler(BookCopyNotAvailable.class)
+	@ResponseStatus(HttpStatus.CONFLICT)
+	public void handleBookCopyNotAvailable(BookCopyNotAvailable e) {
+	}
+}
